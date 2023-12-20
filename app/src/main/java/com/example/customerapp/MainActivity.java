533fe1bc8b9e8774dafec2c0b491905a814a90ff
szpatrik0528 <szpatrik.photoshop@gmail.com>
2023-12-20 09:38:57 +0100

    v1

---
+++ app/src/main/java/com/example/customerapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.customerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView simpleList;
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+    int flags[] = {R.drawable.india, R.drawable.china, R.drawable.australia, R.drawable.portugle, R.drawable.america, R.drawable.new_zealand};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
