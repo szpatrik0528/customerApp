353395c65afbf1cf865577a70a4f61fa302bd6dd
szpatrik0528 <szpatrik.photoshop@gmail.com>
2023-12-20 10:15:51 +0100

    finish

--- app/src/main/java/com/example/customerapp/MainActivity.java
+++ app/src/main/java/com/example/customerapp/MainActivity.java
@@ -14,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+    private void init (){
+        simpleList = (ListView)findViewById(R.id.listView);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), countryList, flags);
+        simpleList.setAdapter(customAdapter);
     }
 }
